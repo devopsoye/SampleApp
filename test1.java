e08944c3bd3fa15832cc32e09e1a359592712d1c
ade <devopsoye@gmail.com>
2021-05-02 12:13:01 +0100

    3rd modification of my script

--- test1.java
+++ test1.java
@@ -4,6 +4,7 @@ class HelloWorld {
 	public static void main(String[] args) {
 		//Prints "Hello, Worlds" to the terminal window
 		System.out.println{"Hello,world"};
-	
-		}
+	        System.out.println{"Git lesson"};
+	        System.out.println{"welcome to Devops"};
+	}
 }
